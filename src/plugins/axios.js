6efae616f017b12602e275a34b691faff093ded5
brunaanjos <brunaanjosdaveiga@gmail.com>
2023-12-11 08:43:33 -0300

    final

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 axios.defaults.baseURL = 'https://api.themoviedb.org/3/'
 
-const token = 'eyJhbGci0iJIUZI1NiJ9.eyJhdWQiOiI3 ODcwYTBiNDcyYzYwNzB1ZWU1ZGQxMmQyM2MyMGE@MCISIn N1YiI6IjY1MDQ20GY4NmEyMjI3MDBmZDIyNDE4YyIsInN jb3BlcyI6WyJhcG1fcmVhZCJdLCJ2ZXJzaW9uIjoxfQ. 4AAm7c6_TRm5sWDVM6B12SWGcsQA6ihqnEcuK1WxIpw'
+const token = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMDZmMWM4Yjg3YzI0NWMyMmVjMDllYmU4ODZjMzQ4MSIsInN1YiI6IjY1MDQ2ODFlNjNhYWQyMDExYjIzNDQzYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.n_vjaEgmee38MnTts1K4Xfc2v6O0vNeirk1FZyU1yiE'
 
 axios.defaults.headers = {
     accept: 'application/json',
-    Authorization: `Bearer ${token}`
+    Authorization: `bearer ${token}`
 }
